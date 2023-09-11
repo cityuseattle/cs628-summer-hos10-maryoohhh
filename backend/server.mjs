2c028e232271866381796fbda3b9ba7b7c0da077
Mary Oh <marychristineoh@gmail.com>
2023-09-11 06:37:30 +0000

    HOS10 submission

--- backend/server.mjs
+++ backend/server.mjs
@@ -3,6 +3,39 @@ import cors from 'cors';
 import "./loadEnvironment.mjs";
 import records from "./routes/record.mjs";
 
+import session from "express-session";
+import passport from "passport";
+import passportFacebook from "passport-facebook";
+
+const FacebookStrategy = passportFacebook.Strategy;
+const FACEBOOK_APP_ID = '880985360029371';
+const FACEBOOK_APP_SECRET = 'b80bb0c2d149baf4eeca6fbdb9379beb';
+
+passport.use(
+    new FacebookStrategy(
+      {
+        clientID: FACEBOOK_APP_ID,
+        clientSecret: FACEBOOK_APP_SECRET,
+        // Your callback URL
+        callbackURL: 'https://humble-space-broccoli-wvxqv96jr5vhvg6w-5050.app.github.dev/facebook/callback', 
+        // Fields you want to access from the user's Facebook profile
+        profileFields: ['id', 'displayName', 'email'], 
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        //logic for using accessToken and RefreshToken
+        return done(null, profile);
+      }
+    )
+  );
+  
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+  });
+
 const PORT = process.env.PORT || 5050;
 const app = express();
 
@@ -10,6 +43,19 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/record", records);
+
+app.use(session({ secret: 'secret-key', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Add Passport Facebook routes
+app.get('/facebook', passport.authenticate('facebook'));
+app.get('/facebook/callback', passport.authenticate('facebook', {
+  // Redirect to the main page upon successful login.
+  successRedirect: 'https://humble-space-broccoli-wvxqv96jr5vhvg6w-3000.app.github.dev/home', 
+  // Redirect to login page on authentication failure.
+  failureRedirect: 'https://humble-space-broccoli-wvxqv96jr5vhvg6w-3000.app.github.dev/', 
+}));
 
 // start the Express server
 app.listen(PORT, () => {
